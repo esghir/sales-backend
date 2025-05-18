11cd6fc6a968489d153289f0a8a4bd9147a33cb0
esghir <esghir.amine.97@gmail.com>
2025-05-18 13:29:00 +0100

    Initial commit - Spring Boot backend setup

---
+++ src/test/java/com/sales/backend/SalesBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sales.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalesBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
